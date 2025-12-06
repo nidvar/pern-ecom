5b1b3d1a7d83cc437443d7889b5837b0d5069133
unknown <djayshine87@hotmail.com>
2025-12-06 13:07:00 +1100

    grab all products from postgres, more controllers added

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -1,13 +1,37 @@
-import type {Request, Response} from 'express'
+import type {Request, Response} from 'express';
+
+import { sql } from '../config/db.js'
 
 export const allProducts = async (req: Request, res: Response)=>{
-    console.log(req.body);
-    console.log('getting all products')
-    res.send({message: 'all products'})
+    console.log('getting all products');
+    try{
+        const products = await sql`
+            SELECT * FROM products
+            ORDER BY created_at DESC
+        `;
+        console.log('products------->', products);
+        res.status(200).json({message: 'all products', data: products});
+    }catch(error){
+        console.log('all products error ======= >',error);
+    }
 };
 
 export const createProduct = async (req: Request, res: Response)=>{
-    console.log(req.body);
     console.log('Creating single product');
     res.send({message: 'creating single product'})
+};
+
+export const singleProduct = async (req: Request, res: Response)=>{
+    console.log('get single product');
+    res.send({message: 'get single product'})
+};
+
+export const updateProduct = async (req: Request, res: Response)=>{
+    console.log('update product');
+    res.send({message: 'update product'})
+};
+
+export const deleteProduct = async (req: Request, res: Response)=>{
+    console.log('delete product');
+    res.send({message: 'delete product'})
 };
--- backend/routes/productRoutes.ts
+++ backend/routes/productRoutes.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 
-import { allProducts, createProduct } from '../controllers/productController.js'
+import { allProducts, createProduct, singleProduct, updateProduct, deleteProduct } from '../controllers/productController.js'
 
 const router = express.Router();
 
 router.get('/allproducts', allProducts);
+router.get('/:id/singleproduct', singleProduct);
 
 router.post('/createproduct', createProduct);
+
+router.put('/:id/updateProduct', updateProduct);
+
+router.delete('/deleteProduct', deleteProduct);
 
 export default router;
