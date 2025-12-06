30e06df37a26c7c175f4055a0fe698bcfae7b06c
unknown <djayshine87@hotmail.com>
2025-12-06 11:49:27 +1100

    removed js files. compiled ts files go to dist folder

--- backend/routes/productRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from 'express';
-import { allProducts } from '../controllers/productController.js';
-const router = express.Router();
-router.get('/allproducts', allProducts);
-export default router;
--- backend/server.ts
+++ backend/server.ts
@@ -7,7 +7,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import cors from 'cors';
 
-import productRoutes from '../backend/routes/productRoutes.js'
+import productRoutes from './routes/productRoutes.js'
+import { sql } from './config/db.js';
 
 const app = express();
 
@@ -16,9 +17,27 @@ app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-
 app.use('/api', productRoutes);
 
-app.listen(PORT, ()=>{
-    console.log('running on port ', PORT);
+async function initDB(){
+    try{
+        await sql`
+            CREATE TABLE IF NOT EXISTS products (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(255) NOT NULL,
+                image VARCHAR(255) NOT NULL,
+                price VARCHAR(255) NOT NULL,
+                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+            )
+        `;
+        console.log('connected to POSTGRES NEON SQL DB')
+    }catch(err){
+        console.log('error', err);
+    }
+};
+
+initDB().then(()=>{
+    app.listen(PORT, ()=>{
+        console.log('running on port ', PORT);
+    });
 });
